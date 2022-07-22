56f19a356d47efe11726408a4d57b1ab6dd983f4
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-07-23 01:00:50 +0300

    feat: +1 (5/8) solutions in functions-n-closures-tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -178,8 +178,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentCount = startFrom - 1;
+
+  return () => {
+    currentCount += 1;
+    return currentCount;
+  };
 }
 
 
